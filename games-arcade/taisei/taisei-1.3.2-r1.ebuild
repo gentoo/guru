gentoo/guru

commit c1a508a5d5d025486fbc9f2912dbefde9ac68f47

games-arcade/taisei: Fix dependencies
- cglm is a library so that goes in DEPEND instead of BDEPEND
- RDEPEND should be defined (and exclude cglm as taisei uses only the headers)

Signed-off-by: Haelwenn (lanodan) Monnier <contact@hacktivis.me>

games-arcade/taisei/taisei-1.3.2-r1.ebuild
@@ -23,18 +23,22 @@ SLOT="0"
 
 IUSE="doc lto zip"
 
-DEPEND="
+RDEPEND="
 	media-libs/freetype:2
 	>=media-libs/libpng-1.5
 	media-libs/libsdl2
-	media-libs/sdl2-mixer[opus]
+	media-libs/opusfile
 	media-libs/libwebp
+	app-arch/zstd
 	sys-libs/zlib
 	zip? ( dev-libs/libzip )
 "
-BDEPEND="
+DEPEND="
+	${RDEPEND}
 	>=dev-libs/cglm-0.7.8
-	>=dev-util/meson-0.49
+"
+BDEPEND="
+	>=dev-util/meson-0.53
 	${PYTHON_DEPS}
 	doc? ( dev-python/docutils )"
 

games-arcade/taisei/taisei-9999.ebuild
@@ -23,7 +23,7 @@ SLOT="0"
 
 IUSE="doc lto zip"
 
-DEPEND="
+RDEPEND="
 	media-libs/freetype:2
 	>=media-libs/libpng-1.5
 	media-libs/libsdl2
@@ -33,8 +33,11 @@ DEPEND="
 	sys-libs/zlib
 	zip? ( dev-libs/libzip )
 "
-BDEPEND="
+DEPEND="
+	${RDEPEND}
 	>=dev-libs/cglm-0.7.8
+"
+BDEPEND="
 	>=dev-util/meson-0.53
 	${PYTHON_DEPS}
 	doc? ( dev-python/docutils )"

commit b75db7ac93a433d1ab6260ea9d19e03308e11f25

games-arcade/taisei: Fix wrong deps introduced in c1a508a5
Closes: https://bugs.gentoo.org/787992
Signed-off-by: Haelwenn (lanodan) Monnier <contact@hacktivis.me>

games-arcade/taisei/taisei-1.3.2-r1.ebuild
@@ -27,9 +27,8 @@ RDEPEND="
 	media-libs/freetype:2
 	>=media-libs/libpng-1.5
 	media-libs/libsdl2
-	media-libs/opusfile
+	media-libs/sdl2-mixer[opus]
 	media-libs/libwebp
-	app-arch/zstd
 	sys-libs/zlib
 	zip? ( dev-libs/libzip )
 "

gui-apps/wdisplays/wdisplays-9999.ebuild
@@ -1,35 +1,0 @@
-# Copyright 1999-2021 Gentoo Authors
-# Distributed under the terms of the GNU General Public License v2
-
-EAPI=7
-
-inherit meson xdg
-
-DESCRIPTION="GUI display configurator for wlroots compositors"
-HOMEPAGE="https://cyclopsian.github.io/wdisplays https://github.com/cyclopsian/wdisplays"
-
-if [[ "${PV}" == 9999 ]]
-then
-	inherit git-r3
-	EGIT_REPO_URI="https://github.com/cyclopsian/wdisplays.git"
-else
-	SRC_URI="https://github.com/cyclopsian/wdisplays/archive/${PV}.tar.gz -> ${P}.tar.gz"
-	KEYWORDS="~amd64 ~arm64 ~x86"
-fi
-
-RDEPEND="
-	dev-libs/glib
-	media-libs/libepoxy
-	x11-libs/gtk+:3[wayland]
-	x11-libs/cairo
-	x11-libs/pango
-"
-DEPEND="${RDEPEND}"
-BDEPEND="
-	dev-libs/wayland-protocols
-"
-
-LICENSE="GPL-3+"
-SLOT="0"
-
-PATCHES=( "${FILESDIR}/${PN}-1.0-pull20.patch" )