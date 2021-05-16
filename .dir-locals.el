926e90a18a21208a4a1fa36c755c818f68b477d0
Maciej Barć <xgqt@riseup.net>
2021-05-17 00:44:27 +0200

    .dir-locals.el: add dir-locals for Emacs
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+(
+ (find-file
+  . (
+     (require-final-newline . t)
+     (show-trailing-whitespace . t)
+     (tab-width . 4)
+     )
+  )
+ (ebuild-mode
+  . (
+     (indent-tabs-mode . t)
+     )
+  )
+ )
