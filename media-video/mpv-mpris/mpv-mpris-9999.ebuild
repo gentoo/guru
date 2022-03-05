gentoo/guru

commit c5daf69b839cb5b11f98c0a0401bc0219724bbd2

media-video/mpv-mpris: add slot operators
* Use `dosym -r` instead of absolute paths

* Use dolib.so instead of doins

Signed-off-by: Anna (cybertailor) Vyalkova <cyber+gentoo@sysrq.in>

media-video/mpv-mpris/mpv-mpris-9999.ebuild
@@ -20,23 +20,21 @@ SLOT="0"
 LICENSE="MIT"
 IUSE="+autoload"
 
-BDEPEND="virtual/pkgconfig"
-RDEPEND="media-video/mpv:=[cplugins,libmpv]
-	dev-libs/glib"
+RDEPEND="
+	dev-libs/glib:2
+	media-video/mpv:=[cplugins,libmpv]
+"
 DEPEND="${RDEPEND}"
-
-DOCS=(
-	README.md
-)
+BDEPEND="virtual/pkgconfig"
 
 src_compile() {
-	emake CC="$(tc-getCC)" PKG_CONFIG="$(tc-getPKG_CONFIG)"
+	tc-export CC
+	emake PKG_CONFIG="$(tc-getPKG_CONFIG)"
 }
 
 src_install() {
-	insinto "/usr/$(get_libdir)/mpv"
-	doins mpris.so
-	use autoload && dosym "/usr/$(get_libdir)/mpv/mpris.so" "/etc/mpv/scripts/mpris.so"
+	dolib.so mpris.so
+	use autoload && dosym -r /usr/$(get_libdir)/mpv/mpris.so /etc/mpv/scripts/mpris.so
 	einstalldocs
 }
 
@@ -45,8 +43,8 @@ pkg_postinst() {
 		elog
 		elog "The plugin has not been installed to /etc/mpv/scripts for autoloading."
 		elog "You have to activate it manually by passing"
-		elog " \"/usr/$(get_libdir)/mpv/mpris.so\" "
-		elog "as script option to mpv or symlinking the library to \"scripts/\" in your mpv"
+		elog " '${EPREFIX}/usr/$(get_libdir)/mpv/mpris.so' "
+		elog "as a script option to mpv or symlinking the library to 'scripts' in your mpv"
 		elog "config directory."
 		elog "Alternatively, activate the autoload use flag."
 		elog

commit 13beddbe34e5e6053341149a49024f6ee11981f0

media-video/mpv-mpris: Fix broken library install path.
This fixes the installation path for the library,
which got broken in:
c5daf69b839cb5b11f98c0a0401bc0219724bbd2
leading to a dangling symlink.

Package-Manager: Portage-3.0.30, Repoman-3.0.3
Signed-off-by: Oliver Freyermuth <o.freyermuth@googlemail.com>

media-video/mpv-mpris/mpv-mpris-9999.ebuild
@@ -51,7 +51,8 @@ src_test() {
 }
 
 src_install() {
-	dolib.so mpris.so
+	insinto "/usr/$(get_libdir)/mpv"
+	doins mpris.so
 	use autoload && dosym -r /usr/$(get_libdir)/mpv/mpris.so /etc/mpv/scripts/mpris.so
 	einstalldocs
 }