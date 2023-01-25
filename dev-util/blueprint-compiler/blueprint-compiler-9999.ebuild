gentoo/guru

commit 5a6292c157f7e0e83b2e97615affb8313ee558e6

dev-util/blueprint-compiler: Update test execution for live ebuild
An X environment is now required to run the tests.

Reference: https://gitlab.gnome.org/jwestman/blueprint-compiler/-/commit/039d88ab45001cf799c421e58d4669a0596c4d29
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

dev-util/blueprint-compiler/blueprint-compiler-9999.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 PYTHON_COMPAT=( python3_{9..11} )
 
-inherit meson python-r1
+inherit meson python-r1 virtualx
 
 if [[ ${PV} == *9999 ]]; then
 	inherit git-r3
@@ -57,7 +57,7 @@ src_compile() {
 }
 
 src_test() {
-	python_foreach_impl meson_src_test
+	virtx python_foreach_impl meson_src_test
 }
 
 src_install() {

commit a1d2a1419061ef902ef9546de91b1de56df49910

dev-util/blueprint-compiler: Update copyright year in live ebuild
Fixes: 5a6292c157f7e0e83b2e97615affb8313ee558e6
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

dev-util/blueprint-compiler/blueprint-compiler-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2022 Gentoo Authors
+# Copyright 2022-2023 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8