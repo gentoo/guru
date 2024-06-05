gentoo/guru

commit 5d0270aaf228fe067d2226de9888908bb23e6a09

net-misc/FORT-validator: declare read-only parameter's type as const
Fixes build failure due to -Wincompatible-pointer-types

Closes: https://bugs.gentoo.org/928331
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

net-misc/FORT-validator/files/FORT-validator-1.5.4-GCC14.patch
@@ -1,0 +1,28 @@
+https://github.com/NICMx/FORT-validator/pull/137
+
+Since we don't manipulate any struct members, I believe we can safely take a
+const xmlError * argument instead, as per libxml2's documentation .
+
+I also believe we can drop the string-termination handling, because C strings
+end with \0 and therefore ptr[strlen(ptr)-1] is always \0.
+
+Author: Lucio Sauer <watermanpaint@posteo.net>
+--- a/src/xml/relax_ng.c
++++ b/src/xml/relax_ng.c
+@@ -36,14 +36,9 @@ relax_ng_log_warn(void *ctx, const char *msg, ...)
+ }
+
+ static void
+-relax_ng_log_str_err(void *userData, xmlErrorPtr error)
++relax_ng_log_str_err(void *userData, const xmlError *error)
+ {
+-       char *ptr;
+-
+-       ptr = error->message;
+-       if (ptr[strlen(ptr) - 1] == '\n')
+-               ptr[strlen(ptr) - 1] = '\0';
+-       pr_val_err("%s (at line %d)", ptr, error->line);
++       pr_val_err("%s (at line %d)", error->message, error->line);
+ }
+
+ /* Initialize global schema to parse RRDP files */

net-misc/FORT-validator/FORT-validator-1.5.4-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2020-2021 Gentoo Authors
+# Copyright 2020-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -18,18 +18,22 @@ SLOT="0"
 KEYWORDS="~amd64 ~x86"
 IUSE="caps"
 
+PATCHES="${FILESDIR}"/${PN}-1.5.4-GCC14.patch
+
 DEPEND="
 	acct-group/fort
 	acct-user/fort
 	caps? ( sys-libs/libcap )
 	dev-libs/jansson
-	dev-libs/libxml2
 	dev-libs/openssl
-	net-misc/curl
 "
 RDEPEND="
 	${DEPEND}
 	net-misc/rsync
+"
+BDEPEND="
+	dev-build/automake
+	dev-build/automake
 "
 
 src_prepare() {
@@ -38,6 +42,8 @@ src_prepare() {
 	sed -i 's/fort_CFLAGS  =/fort_CFLAGS  = ${CFLAGS} /' src/Makefile.am || die
 	# Don't test network
 	sed -i '/http/d' test/Makefile.am || die
+	# Don’t compile debug by default
+	sed -i '/fort_CFLAGS/ s/ -g / /' src/Makefile.am || die
 	eautoreconf
 }
 

net-misc/FORT-validator/FORT-validator-1.6.1-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2020-2021 Gentoo Authors
+# Copyright 2020-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -18,20 +18,20 @@ SLOT="0"
 KEYWORDS="~amd64 ~x86"
 IUSE="caps"
 
+PATCHES="${FILESDIR}"/${PN}-1.5.4-GCC14.patch
+
 DEPEND="
 	acct-group/fort
 	acct-user/fort
 	caps? ( sys-libs/libcap )
 	dev-libs/jansson
+	dev-libs/libxml2
 	dev-libs/openssl
+	net-misc/curl
 "
 RDEPEND="
 	${DEPEND}
 	net-misc/rsync
-"
-BDEPEND="
-	dev-build/automake
-	dev-build/automake
 "
 
 src_prepare() {
@@ -40,8 +40,6 @@ src_prepare() {
 	sed -i 's/fort_CFLAGS  =/fort_CFLAGS  = ${CFLAGS} /' src/Makefile.am || die
 	# Don't test network
 	sed -i '/http/d' test/Makefile.am || die
-	# Don’t compile debug by default
-	sed -i '/fort_CFLAGS/ s/ -g / /' src/Makefile.am || die
 	eautoreconf
 }
 

net-misc/FORT-validator/FORT-validator-1.6.2-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2020-2021 Gentoo Authors
+# Copyright 2020-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -15,6 +15,8 @@ LICENSE="MIT"
 SLOT="0"
 KEYWORDS="~amd64 ~x86"
 IUSE="caps"
+
+PATCHES="${FILESDIR}"/${PN}-1.5.4-GCC14.patch
 
 DEPEND="
 	acct-group/fort

commit 0e9bffaf0c73a064861831d8081da3047f9b2862

net-misc/FORT-validator: restore compat with older libxml2 versions
5d0270aaf228fe067d2226de9888908bb23e6a09 broke compatibility with
<dev-libs/libxml2-2.12.0 due to API changes.
Backport commits that fix previous patch.

Bug: https://bugs.gentoo.org/928331
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

net-misc/FORT-validator/FORT-validator-1.5.4-r2.ebuild
@@ -19,16 +19,14 @@ KEYWORDS="~amd64 ~x86"
 IUSE="caps test"
 RESTRICT="!test? ( test )"
 
-PATCHES="${FILESDIR}"/${PN}-1.5.4-GCC14.patch
+PATCHES="${FILESDIR}"/${PN}-1.5.4-libxml2-2.12.0.patch
 
 DEPEND="
 	acct-group/fort
 	acct-user/fort
 	caps? ( sys-libs/libcap )
 	dev-libs/jansson
-	dev-libs/libxml2
-	dev-libs/openssl[rfc3779]
-	net-misc/curl
+	dev-libs/openssl
 "
 RDEPEND="${DEPEND}"
 BDEPEND="
@@ -42,6 +40,8 @@ src_prepare() {
 	sed -i 's/fort_CFLAGS  =/fort_CFLAGS  = ${CFLAGS} /' src/Makefile.am || die
 	# Don't test network
 	sed -i '/http/d' test/Makefile.am || die
+	# Don’t compile debug by default
+	sed -i '/fort_CFLAGS/ s/ -g / /' src/Makefile.am || die
 	eautoreconf
 }
 

net-misc/FORT-validator/FORT-validator-1.6.1-r2.ebuild
@@ -19,14 +19,16 @@ KEYWORDS="~amd64 ~x86"
 IUSE="caps test"
 RESTRICT="!test? ( test )"
 
-PATCHES="${FILESDIR}"/${PN}-1.5.4-GCC14.patch
+PATCHES="${FILESDIR}"/${PN}-1.5.4-libxml2-2.12.0.patch
 
 DEPEND="
 	acct-group/fort
 	acct-user/fort
 	caps? ( sys-libs/libcap )
 	dev-libs/jansson
+	dev-libs/libxml2
 	dev-libs/openssl
+	net-misc/curl
 "
 RDEPEND="${DEPEND}"
 BDEPEND="
@@ -40,8 +42,6 @@ src_prepare() {
 	sed -i 's/fort_CFLAGS  =/fort_CFLAGS  = ${CFLAGS} /' src/Makefile.am || die
 	# Don't test network
 	sed -i '/http/d' test/Makefile.am || die
-	# Don’t compile debug by default
-	sed -i '/fort_CFLAGS/ s/ -g / /' src/Makefile.am || die
 	eautoreconf
 }
 

net-misc/FORT-validator/FORT-validator-1.6.2-r2.ebuild
@@ -19,7 +19,7 @@ KEYWORDS="~amd64 ~x86"
 IUSE="caps test"
 RESTRICT="!test? ( test )"
 
-PATCHES="${FILESDIR}"/${PN}-1.5.4-GCC14.patch
+PATCHES="${FILESDIR}"/${PN}-1.5.4-libxml2-2.12.0.patch
 
 DEPEND="
 	acct-group/fort
@@ -27,7 +27,7 @@ DEPEND="
 	caps? ( sys-libs/libcap )
 	dev-libs/jansson
 	dev-libs/libxml2
-	dev-libs/openssl
+	dev-libs/openssl[rfc3779]
 	net-misc/curl
 "
 RDEPEND="${DEPEND}"

net-misc/FORT-validator/files/FORT-validator-1.5.4-GCC14.patch
@@ -1,28 +1,0 @@
-https://github.com/NICMx/FORT-validator/pull/137
-
-Since we don't manipulate any struct members, I believe we can safely take a
-const xmlError * argument instead, as per libxml2's documentation .
-
-I also believe we can drop the string-termination handling, because C strings
-end with \0 and therefore ptr[strlen(ptr)-1] is always \0.
-
-Author: Lucio Sauer <watermanpaint@posteo.net>
---- a/src/xml/relax_ng.c
-+++ b/src/xml/relax_ng.c
-@@ -36,14 +36,9 @@ relax_ng_log_warn(void *ctx, const char *msg, ...)
- }
-
- static void
--relax_ng_log_str_err(void *userData, xmlErrorPtr error)
-+relax_ng_log_str_err(void *userData, const xmlError *error)
- {
--       char *ptr;
--
--       ptr = error->message;
--       if (ptr[strlen(ptr) - 1] == '\n')
--               ptr[strlen(ptr) - 1] = '\0';
--       pr_val_err("%s (at line %d)", ptr, error->line);
-+       pr_val_err("%s (at line %d)", error->message, error->line);
- }
-
- /* Initialize global schema to parse RRDP files */

net-misc/FORT-validator/files/FORT-validator-1.5.4-libxml2-2.12.0.patch
@@ -1,0 +1,31 @@
+Backport commits 58a94fc..cdf9248 that enable compatibility with
+>=dev-libs/libxml2-2.12.0.
+
+Author: Lucio Sauer <watermanpaint@posteo.net>
+--- a/src/xml/relax_ng.c
++++ b/src/xml/relax_ng.c
+@@ -35,15 +35,17 @@ relax_ng_log_warn(void *ctx, const char *msg, ...)
+ 	VLOG_MSG(warn)
+ }
+ 
++/* Signature changed at libxml2 commit 61034116d0a3c8b295c6137956adc3ae55720. */
++#if LIBXML_VERSION >= 21200
++#define XMLERROR_PARAMTYPE const xmlError *
++#else
++#define XMLERROR_PARAMTYPE xmlErrorPtr
++#endif
++
+ static void
+-relax_ng_log_str_err(void *userData, xmlErrorPtr error)
++relax_ng_log_str_err(void *userData, XMLERROR_PARAMTYPE error)
+ {
+-	char *ptr;
+-
+-	ptr = error->message;
+-	if (ptr[strlen(ptr) - 1] == '\n')
+-		ptr[strlen(ptr) - 1] = '\0';
+-	pr_val_err("%s (at line %d)", ptr, error->line);
++	pr_val_err("%s (at line %d)", error->message, error->line);
+ }
+ 
+ /* Initialize global schema to parse RRDP files */