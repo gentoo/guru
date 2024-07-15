gentoo/guru

commit 9af8c0ed33a761046fa66379429bd38c80590387

net-wireless/btscreenlocker:
* Removed unnecesary function implementations.
* Removed unnecesary dependency on a system package.
* Added required dependency on virtual/pkgconfig.

Signed-off-by: Brayan M. Salazar <this.brayan@proton.me>

net-wireless/btscreenlocker/btscreenlocker-1.6.1.ebuild
@@ -3,8 +3,6 @@
 
 EAPI=8
 
-inherit cmake
-
 DESCRIPTION="Daemon to lock your screen when Bluetooth trusted devices go away."
 HOMEPAGE="https://github.com/brookiestein/BtScreenLocker"
 SRC_URI="https://github.com/brookiestein/BtScreenLocker/archive/refs/tags/${PV}.tar.gz -> BtScreenLocker-${PV}.tar.gz"
@@ -23,18 +21,6 @@ dev-qt/linguist-tools:5
 "
 RDEPEND="${DEPEND}"
 BDEPEND="
-sys-apps/coreutils
+virtual/pkgconfig
 >=dev-build/cmake-3.28
 "
-
-src_configure() {
-	cmake_src_configure || die "Couldn't configure ${PN}."
-}
-
-src_compile() {
-	cmake_build || die "Couldn't compile ${PN}."
-}
-
-src_install() {
-	cmake_src_install || die "Couldn't install ${PN}."
-}

commit 83319890e0ee50137a7471d4764cb2c7e97f3e7f

net-wireless/btscreenlocker: restore inherit cmake
Fixes: 9af8c0ed33a761046fa66379429bd38c80590387
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

net-wireless/btscreenlocker/btscreenlocker-1.6.1.ebuild
@@ -3,6 +3,8 @@
 
 EAPI=8
 
+inherit cmake
+
 DESCRIPTION="Daemon to lock your screen when Bluetooth trusted devices go away."
 HOMEPAGE="https://github.com/brookiestein/BtScreenLocker"
 SRC_URI="https://github.com/brookiestein/BtScreenLocker/archive/refs/tags/${PV}.tar.gz -> BtScreenLocker-${PV}.tar.gz"