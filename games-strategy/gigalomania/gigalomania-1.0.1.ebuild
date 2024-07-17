gentoo/guru

commit 96d99c5656ca40fbdb3a4970130cdfe4f9d6e69a

games-strategy/gigalomania: CCFLAGS from hardcode in Makefile
Closes: https://bugs.gentoo.org/933592
Signed-off-by: Vitaly Zdanevich <zdanevich.vitaly@ya.ru>

games-strategy/gigalomania/gigalomania-1.0.1.ebuild
@@ -29,6 +29,16 @@ RDEPEND="
 	media-libs/sdl2-mixer
 "
 
+PATCHES="
+	${FILESDIR}/Makefile.patch
+"
+
+src_prepare() {
+	ls -lh
+
+	default
+}
+
 src_compile() {
 	emake CFLAGS="${CFLAGS}"
 }

games-strategy/gigalomania/files/Makefile.patch
@@ -1,0 +1,35 @@
+diff --git i/Makefile w/Makefile
+index 7d3056a..a15ef37 100644
+--- i/Makefile
++++ w/Makefile
+@@ -1,28 +1,27 @@
+ CC=g++
+-CCFLAGS=-O2 -Wall
+ CFILES=game.cpp gamestate.cpp gui.cpp image.cpp main.cpp panel.cpp player.cpp resources.cpp screen.cpp sector.cpp sound.cpp tutorial.cpp utils.cpp TinyXML/tinyxml.cpp TinyXML/tinyxmlerror.cpp TinyXML/tinyxmlparser.cpp
+ HFILES=game.h gamestate.h gui.h image.h panel.h player.h resources.h screen.h sector.h sound.h tutorial.h utils.h common.h stdafx.h TinyXML/tinyxml.h
+ OFILES=game.o gamestate.o gui.o image.o panel.o player.o resources.o screen.o sector.o sound.o tutorial.o utils.o main.o TinyXML/tinyxml.o TinyXML/tinyxmlerror.o TinyXML/tinyxmlparser.o
+ APP=gigalomania
+ INC=`sdl2-config --cflags`
+ LINKPATH=`sdl2-config --libs` -L/usr/X11R6/lib/ -L/usr/lib
+ 
+ LIBS=-lSDL2_image -lSDL2_mixer
+ 
+ all: $(APP)
+ 
+ $(APP): $(OFILES) $(HFILES) $(CFILES)
+-	$(CC) $(OFILES) $(CCFLAGS) $(LINKPATH) $(LIBS) -o $(APP)
++	$(CC) $(OFILES) ${CXXFLAGS} $(LINKPATH) $(LIBS) -o $(APP)
+ 
+ .cpp.o:
+-	$(CC) $(CCFLAGS) -O2 $(INC) -c $< -o $@
++	$(CC) ${CXXFLAGS} $(INC) -c $< -o $@
+ 
+ # REMEMBER to update debian/dirs if the system directories that we use are changed!!!
+ install: $(APP)
+ 	mkdir -p $(DESTDIR)/opt/gigalomania # -p so we don't fail if folder already exists
+ 	cp $(APP) $(DESTDIR)/opt/gigalomania
+ 	cp readme.html $(DESTDIR)/opt/gigalomania
+ 	cp -a gfx/ $(DESTDIR)/opt/gigalomania # -a need to copy permissions etc
+ 	cp -a islands/ $(DESTDIR)/opt/gigalomania
+ 	cp -a music/ $(DESTDIR)/opt/gigalomania
+ 	cp -a sound/ $(DESTDIR)/opt/gigalomania

commit 96efefb928a718e465e9ece30e4ddd439553e77d

games-strategy/gigalomania: pass CXXFLAGS instead of CFLAGS to make
This is a cpp program.

Bug: https://bugs.gentoo.org/933592
Fixes: 96d99c5656ca40fbdb3a4970130cdfe4f9d6e69a
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

games-strategy/gigalomania/gigalomania-1.0.1.ebuild
@@ -35,7 +35,7 @@ PATCHES="
 
 src_compile() {
 	tc-export CXX
-	emake CFLAGS="${CFLAGS}"
+	emake CXXFLAGS="${CXXFLAGS}"
 }
 
 src_install() {