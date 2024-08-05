gentoo/guru

commit 3a9d31eb7afeed532ebba926a57e7beff69910cc

dev-tex/tabularray: update SRC_URI and checksum
- switch to GitHub-generated tarballs
- fix tar ball checksum

Closes: https://bugs.gentoo.org/937319
Signed-off-by: Huang Rui <vowstar@gmail.com>

dev-tex/tabularray/tabularray-2023.03.01.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2024 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -7,9 +7,9 @@ inherit latex-package
 
 DESCRIPTION="Typeset tabulars and arrays with LATEX3"
 HOMEPAGE="https://www.ctan.org/pkg/tabularray/"
-SRC_URI="https://mirrors.ctan.org/macros/latex/contrib/tabularray.zip -> ${P}.zip"
+SRC_URI="https://github.com/lvjr/tabularray/archive/refs/tags/2023A.tar.gz -> ${P}.tar.gz"
 
-S="${WORKDIR}/${PN}"
+S="${WORKDIR}/${PN}-2023A"
 
 LICENSE="LPPL-1.3c"
 SLOT="0"

dev-tex/tabularray/Manifest
@@ -1,1 +1,1 @@
-DIST tabularray-2023.03.01.zip 817801 BLAKE2B c80884f998e2c6c6406f8ae5d047b048864c135bdec65b7b2bd65f1e6c8a97fcebb13cf72e615c52a5bb77ac51da77d7a3fbd119c135698b40f9cee90883ff10 SHA512 ef0c67ccd6c90f22df61ce2a2e3b54a329dd7124e071985e696033754275e20ff07301bdf1a6e384c009d6462c2eb0bdc5034973697778b3b25e1e89f10e8c20
+DIST tabularray-2023.03.01.tar.gz 3270836 BLAKE2B 1cc854e1251a263b5fbc48fd0f06acb7e371ba99121a010f6a7c667805074d502c2482f237b817998923f7da84cce3beaa5dacb9d11ec182bf7a058849d1790d SHA512 3651febc21374192e2ffa4fec12e9ece0614cb32454fd277189518df30a574acf024abbfe4a9db6a548d56d7ae508de45d3b255ee47d74b82aebf1ad38a9020c

dev-tex/tabularray/metadata.xml
@@ -5,6 +5,9 @@
 		<email>vowstar@gmail.com</email>
 		<name>Huang Rui</name>
 	</maintainer>
+	<upstream>
+		<remote-id type="github">lvjr/tabularray</remote-id>
+	</upstream>
 	<longdescription lang="en">
 		LATEX tables are implemented using TEX commands such as \halign,
 		\noalign, \span, and \omit. In order to implement new features, many

commit 02300c4a1050b47962d84e1e1a1ccfe4f9cb1dd4

dev-tex/tabularray: remove unzip dep following move to tarball source
Fixes: 3a9d31eb7afeed532ebba926a57e7beff69910cc
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

dev-tex/tabularray/tabularray-2023.03.01.ebuild
@@ -23,10 +23,7 @@ RDEPEND="
 	dev-texlive/texlive-plaingeneric
 "
 DEPEND="${RDEPEND}"
-BDEPEND="
-	${RDEPEND}
-	app-arch/unzip
-"
+BDEPEND="${RDEPEND}"
 
 TEXMF="/usr/share/texmf-site"
 