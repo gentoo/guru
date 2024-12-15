gentoo/guru

commit 08f413c3871d931d1a3fd7ae68dd4c97cc5a308a

games-emulation/dosbox-x: Set XDG_CONFIG_HOME for tests
Since upstream commit 25f23a25c (Log loaded conf file, 2024-11-09),
DOSBox-X creates a default configuration file if an existing file does
not exist when its binary is launched, even when it is just to run self
tests.  When Portage runs the tests, to ensure the tests can start
without an error or even a segmentation fault, the 'portage' user must
have permission to the path where DOSBox-X will create the configuration
file.

Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

games-emulation/dosbox-x/dosbox-x-9999.ebuild
@@ -151,6 +151,7 @@ src_compile() {
 }
 
 src_test() {
+	local -x XDG_CONFIG_HOME="${T}" # Tests may create configuration files
 	set -- src/dosbox-x -tests
 	echo "${@}" >&2
 	"${@}" || die "Unit tests failed"

commit 215e297f203df8291df1fbe4a55a803a0a23104d

games-emulation/dosbox-x: Use xdg-utils.eclass to set XDG_CONFIG_HOME
Suggested-by: Anna (cybertailor) Vyalkova <cyber+gentoo@sysrq.in>
Fixes: 08f413c38 (games-emulation/dosbox-x: Set XDG_CONFIG_HOME for tests)
Signed-off-by: Yuan Liao <liaoyuan@gmail.com>

games-emulation/dosbox-x/dosbox-x-2024.12.04.ebuild
@@ -151,7 +151,7 @@ src_compile() {
 }
 
 src_test() {
-	local -x XDG_CONFIG_HOME="${T}" # Tests may create configuration files
+	xdg_environment_reset # Tests may create config files in XDG_CONFIG_HOME
 	set -- src/dosbox-x -tests
 	echo "${@}" >&2
 	"${@}" || die "Unit tests failed"

games-emulation/dosbox-x/dosbox-x-9999.ebuild
@@ -151,7 +151,7 @@ src_compile() {
 }
 
 src_test() {
-	local -x XDG_CONFIG_HOME="${T}" # Tests may create configuration files
+	xdg_environment_reset # Tests may create config files in XDG_CONFIG_HOME
 	set -- src/dosbox-x -tests
 	echo "${@}" >&2
 	"${@}" || die "Unit tests failed"