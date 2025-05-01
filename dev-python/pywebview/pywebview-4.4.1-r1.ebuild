gentoo/guru

commit 1f6064e8181fecf1607154b873d8d226158f189e

dev-python/pywebview: enable py3.12, py3.13 and disable py3.10
Signed-off-by: Jens Mühlenhoff <j.muehlenhoff@gmx.de>

dev-python/pywebview/pywebview-4.4.1-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{10..11} )
+PYTHON_COMPAT=( python3_{11..13} )
 
 inherit distutils-r1 pypi
 

dev-python/pywebview/pywebview-5.1-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{10..11} )
+PYTHON_COMPAT=( python3_{11..13} )
 
 inherit distutils-r1 pypi
 

commit ec6fd88e1ae5746c0b19c13c08b9ef74245f40ee

dev-python/pywebview: disable py3.13
Partially reverts commit 1f6064e8181fecf1607154b873d8d226158f189e.

This clearly, clearly, clearly wasn't tested. Its dependencies aren't
built or buildable for py3.13 so how could the author of that commit
have been able to run the package for that version?

Fixes: 1f6064e8181fecf1607154b873d8d226158f189e
Signed-off-by: Eli Schwartz <eschwartz@gentoo.org>

dev-python/pywebview/pywebview-4.4.1-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{11..13} )
+PYTHON_COMPAT=( python3_{11..12} )
 
 inherit distutils-r1 pypi
 

dev-python/pywebview/pywebview-5.1-r1.ebuild
@@ -4,7 +4,7 @@
 EAPI=8
 
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{11..13} )
+PYTHON_COMPAT=( python3_{11..12} )
 
 inherit distutils-r1 pypi
 