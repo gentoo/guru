gentoo/guru

commit 8666a4167b23c6dae66e10b82602863968fa5cb4

gui-apps/wayfreeze: new package, add 9999
Signed-off-by: Willem Grant <willemgrant@mailfence.com>

gui-apps/wayfreeze/wayfreeze-9999.ebuild
@@ -1,0 +1,33 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit cargo git-r3
+
+DESCRIPTION="Tool to freeze the screen of a Wayland compositor "
+HOMEPAGE="https://github.com/Jappie3/wayfreeze"
+EGIT_REPO_URI="https://github.com/Jappie3/wayfreeze.git"
+DOCS=( README.md )
+
+LICENSE="AGPL-3"
+SLOT="0"
+KEYWORDS=""
+
+DEPEND="x11-libs/libxkbcommon"
+RDEPEND="${DEPEND}"
+BDEPEND="virtual/rust"
+
+src_unpack() {
+    git-r3_src_unpack
+    cargo_live_src_unpack
+}
+
+src_compile() {
+    cargo_src_compile
+}
+
+src_install() {
+    dobin target/release/wayfreeze
+    einstalldocs
+}

commit ad8765a76ba14b677a1f4f44ff12bad2a94ee13c

gui-apps/wayfreeze: fix whitespace
Fixes: 8666a4167b23c6dae66e10b82602863968fa5cb4
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

gui-apps/wayfreeze/wayfreeze-9999.ebuild
@@ -19,15 +19,15 @@ RDEPEND="${DEPEND}"
 BDEPEND="dev-lang/rust"
 
 src_unpack() {
-    git-r3_src_unpack
-    cargo_live_src_unpack
+	git-r3_src_unpack
+	cargo_live_src_unpack
 }
 
 src_compile() {
-    cargo_src_compile
+	cargo_src_compile
 }
 
 src_install() {
-    dobin target/release/wayfreeze
-    einstalldocs
+	dobin target/release/wayfreeze
+	einstalldocs
 }