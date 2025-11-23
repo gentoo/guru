gentoo/guru

commit 26eb58c5026cf1acbc0898d1d94d873fc579e7c9

app-editors/xed: new package, add 3.8.4
Signed-off-by: Antonis Tatmichalis <antonistatmichalis@outlook.com>

app-editors/xed/metadata.xml
@@ -1,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
+<pkgmetadata>
+	<maintainer type="person">
+		<email>antonistatmichalis@outlook.com</email>
+		<name>Antonis Tatmichalis</name>
+	</maintainer>
+        <upstream>
+                <remote-id type="github">linuxmint/xed</remote-id>
+        </upstream>
+</pkgmetadata>

app-editors/xed/xed-3.8.4.ebuild
@@ -1,0 +1,52 @@
+# Copyright 1999-2025 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+inherit meson xdg-utils
+
+DESCRIPTION="Small text editor by the Linux Mint developers"
+HOMEPAGE="https://github.com/linuxmint/xed"
+SRC_URI="https://github.com/linuxmint/xed/archive/${PV}.tar.gz -> ${P}.tar.gz"
+
+LICENSE="GPL-2"
+SLOT="0"
+KEYWORDS="~amd64"
+IUSE="gtk-doc spell"
+
+RDEPEND="
+        >=dev-libs/libxml2-2.5.0:=
+        >=dev-libs/glib-2.40.0
+        >=x11-libs/gtk+-3.19.3:=
+        >=x11-libs/gtksourceview-4.0.3
+        >=dev-libs/libpeas-1.12.0
+        x11-libs/pango
+        x11-libs/libxklavier
+        >=x11-libs/xapp-1.9.0
+        spell? ( >=app-text/gspell-0.2.5 )
+        gtk-doc? ( dev-util/gtk-doc )
+"
+DEPEND="${RDEPEND}"
+BDEPEND="virtual/pkgconfig"
+
+src_configure() {
+	local emesonargs=(
+		$(meson_use spell enable_spell)
+		$(meson_use gtk-doc docs)
+	)
+	meson_src_configure
+}
+
+src_install() {
+	meson_src_install
+}
+
+pkg_postinst() {
+	xdg_desktop_database_update
+	xdg_icon_cache_update
+}
+
+pkg_postrm() {
+	xdg_desktop_database_update
+	xdg_icon_cache_update
+}

app-editors/xed/Manifest
@@ -1,0 +1,1 @@
+DIST xed-3.8.4.tar.gz 4603399 BLAKE2B fe311f47850615a14077904af8beeba11b054f849f399f2916b7338dd3b5bc7e29a71cde70d32d1201b01bbca12ef4417cf2078db038a747aa21281c0d8b204a SHA512 f443803ea07f5d35f9c1925dac76037750df9b9135c32746b8e0ef175c6b75ebcd92f5853bc94ced1f52a01d0a1f3c57a3ae1a58cb45a8ca98464459dbca8b2b

commit 6963a16eb1d0743002e763ee25377644a0357acf

app-editors/xed: fix whitespace
Fixes: 26eb58c5026cf1acbc0898d1d94d873fc579e7c9
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

app-editors/xed/metadata.xml
@@ -5,7 +5,7 @@
 		<email>antonistatmichalis@outlook.com</email>
 		<name>Antonis Tatmichalis</name>
 	</maintainer>
-        <upstream>
-                <remote-id type="github">linuxmint/xed</remote-id>
-        </upstream>
+	<upstream>
+		<remote-id type="github">linuxmint/xed</remote-id>
+	</upstream>
 </pkgmetadata>

app-editors/xed/xed-3.8.4.ebuild
@@ -15,16 +15,16 @@ KEYWORDS="~amd64"
 IUSE="gtk-doc spell"
 
 RDEPEND="
-        >=dev-libs/libxml2-2.5.0:=
-        >=dev-libs/glib-2.40.0
-        >=x11-libs/gtk+-3.19.3:=
-        >=x11-libs/gtksourceview-4.0.3
-        >=dev-libs/libpeas-1.12.0
-        x11-libs/pango
-        x11-libs/libxklavier
-        >=x11-libs/xapp-1.9.0
-        spell? ( >=app-text/gspell-0.2.5 )
-        gtk-doc? ( dev-util/gtk-doc )
+	>=dev-libs/libxml2-2.5.0:=
+	>=dev-libs/glib-2.40.0
+	>=x11-libs/gtk+-3.19.3:=
+	>=x11-libs/gtksourceview-4.0.3
+	>=dev-libs/libpeas-1.12.0
+	x11-libs/pango
+	x11-libs/libxklavier
+	>=x11-libs/xapp-1.9.0
+	spell? ( >=app-text/gspell-0.2.5 )
+	gtk-doc? ( dev-util/gtk-doc )
 "
 DEPEND="${RDEPEND}"
 BDEPEND="virtual/pkgconfig"