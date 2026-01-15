gentoo/guru

commit c1fc82fc0431bb9057915bfb2faab85bbc0c25df

sec-keys/openpgp-keys-google-artifact-registry: new package, add 20210504
Signed-off-by: Florian Albrechtskirchinger <falbrechtskirchinger@gmail.com>

sec-keys/openpgp-keys-google-artifact-registry/openpgp-keys-google-artifact-registry-20210504.ebuild
@@ -1,0 +1,19 @@
+# Copyright 2026 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+EAPI=8
+
+SEC_KEYS_VALIDPGPKEYS=(
+	"35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3:artifact-registry-repository-signer:manual"
+)
+
+inherit sec-keys
+
+DESCRIPTION="Google Cloud Artifact Registry Automatic Signing Key"
+HOMEPAGE="https://docs.cloud.google.com/artifact-registry/docs"
+SRC_URI="https://packages.cloud.google.com/apt/doc/apt-key.gpg -> ${P}.asc"
+S="${WORKDIR}"
+
+LICENSE="public-domain"
+SLOT="0"
+KEYWORDS="amd64 arm64"

sec-keys/openpgp-keys-google-artifact-registry/Manifest
@@ -1,0 +1,1 @@
+DIST openpgp-keys-google-artifact-registry-20210504.asc 1022 BLAKE2B 28993a0ce673816857a0d5058b8c60ef95b74e185cc4a7dadf890a674e84cec85ece58602eb973fe4723b68c6babcc0e35a2e1c7eb7df14189f4508d1c93b27b SHA512 03a12936bc21e2bd6ecca5eada381e11d6ed61e6e8d6dafab1346c0bf5387e663bd3b93440dcbfe0b7829b1a900b2553420029142ecfe319e8cb226af712d248

sec-keys/openpgp-keys-google-artifact-registry/metadata.xml
@@ -1,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
+<pkgmetadata>
+  <maintainer type="person">
+    <email>falbrechtskirchinger@gmail.com</email>
+    <name>Florian Albrechtskirchinger</name>
+  </maintainer>
+</pkgmetadata>

commit cdca250cd2229bb2be1952131ba6d929c0863371

sec-keys/openpgp-keys-google-artifact-registry: destabilize 20210504 for ~amd64, ~arm64
Fixes: c1fc82fc0431bb9057915bfb2faab85bbc0c25df
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

sec-keys/openpgp-keys-google-artifact-registry/openpgp-keys-google-artifact-registry-20210504.ebuild
@@ -16,4 +16,4 @@ S="${WORKDIR}"
 
 LICENSE="public-domain"
 SLOT="0"
-KEYWORDS="amd64 arm64"
+KEYWORDS="~amd64 ~arm64"