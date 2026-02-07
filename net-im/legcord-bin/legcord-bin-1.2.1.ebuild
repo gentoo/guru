gentoo/guru

commit 4712478547fb3f23ec2e59fe65cf235a1c29aba9

net-im/legcord-bin: add 1.2.0, drop 1.1.6-r1
+metainfo

Signed-off-by: dsaf <ghostyn678+git@gmail.com>

net-im/legcord-bin/Manifest
@@ -1,2 +1,3 @@
-DIST legcord-bin-1.1.6-amd64.deb 105068616 BLAKE2B 330c40ef2c5753fdf50dc45a4073569f90d5d0caf9a388263d050006950d570c3f56daf4c98c462ce1966fe6ef6200185640dc4fb9a8fd0585b0ded0dcb99bb4 SHA512 9911ea722de2a561e61eb5e5da87152899f9023520025622219b9b1ab779f0b68178385daf4f8ce8ecb46afab4a52b39c52c94946eea93d49d182a256bcad02e
-DIST legcord-bin-1.1.6-arm64.deb 99729972 BLAKE2B 915b26a084476f14a6e0e11279f549d5aa906f0ab4274435da1213ac174080a190940833a9acc86000fbb3744fc9534c39c0da84c591c4661be897f7e2c5cde1 SHA512 fa1509c0639d3ee81df7fc57a9bac6f1aacd9aea7ecfd36e4812cc967f7b9d8ce04484435a7cc4711a99f1758014459e018e01cae706531aedd478bbfe95f5a0
+DIST app.legcord.Legcord.metainfo.xml 10474 BLAKE2B 0fe145c83ce1beb5359a0f013ed72cc1297197d923918fbd37def063f1efc9f4de319ba4753837f65290bfdf368c789dd49ea401dc033f5a4abb14f1a5bd93a2 SHA512 b2046711d2f9fedd0d756e7c43957f93f3c3a2d894f125d9b4a2d7c75b33bae010254e442d9badcc6245f9478f36c781b90d047b273b740e96d3d7aba4d51236
+DIST legcord-bin-1.2.0-amd64.deb 108193760 BLAKE2B c8cab2b4d482f703f7b7d712a5b4f08ef9c1461da7fd9248672d1f7a6fe16090bcd29fc8486e2cdff74984366ef10ad8072121a883069ebac7d9961f7239e358 SHA512 640a33c7aa3f3e64212394e15eff3cc69d1a9b7662300d1770e95267fe5f927a762aa181dc36d369d2912ebed3246050b6f62aa15bc7e4118683265888cfc6eb
+DIST legcord-bin-1.2.0-arm64.deb 103149036 BLAKE2B d37324a1555ea6e0615865e50c04d2f9a6dd029a6dc93217728cce208653699e7a722bfe8d8d622b329945d734905de5664f602cce7a1fe32baa04418d756c2d SHA512 d8975203c9fe3b5158c9dad149e042115a4b18edb555637a56b39187a01731c6d442b1567ebfe1ebf4edfcc138f401d00138a150e602682b58b49c2a76bfaed6

net-im/legcord-bin/legcord-bin-1.2.0.ebuild
@@ -14,6 +14,8 @@ SRC_URI="
 	amd64? ( https://github.com/Legcord/Legcord/releases/download/v${PV}/Legcord-${PV}-linux-amd64.deb -> ${P}-amd64.deb )
 	arm64? ( https://github.com/Legcord/Legcord/releases/download/v${PV}/Legcord-${PV}-linux-arm64.deb -> ${P}-arm64.deb )
 "
+# metainfo
+SRC_URI+="https://github.com/Legcord/Legcord/releases/download/v${PV}/app.legcord.Legcord.metainfo.xml"
 
 S="${WORKDIR}"
 
@@ -50,7 +52,8 @@ RDEPEND="
 QA_PREBUILT="*"
 
 src_unpack() {
-	unpack_deb ${A}
+	use amd64 && unpack_deb "${P}-amd64.deb"
+	use arm64 && unpack_deb "${P}-arm64.deb"
 }
 
 src_prepare() {
@@ -71,6 +74,9 @@ src_install() {
 
 	domenu "usr/share/applications/${MY_PN}.desktop"
 
+	insinto /usr/share/metainfo
+	doins "${DISTDIR}/app.legcord.${MY_PN}.metainfo.xml"
+
 	exeinto "${DESTDIR}"
 	doexe "opt/${MY_PN}/${MY_PN}"
 	doexe "opt/${MY_PN}/chrome-sandbox"
@@ -83,19 +89,8 @@ src_install() {
 
 	doins "opt/${MY_PN}/icudtl.dat"
 
-	#insopts -m0766
 	doins -r "opt/${MY_PN}/locales"
 	doins -r "opt/${MY_PN}/resources"
-
-	#fperms -R 644 "${DESTDIR}/locales"
-	#fperms -R 644 "${DESTDIR}/resources"
-
-	# Fix bug 930639
-	#fperms -R a+r "${DESTDIR}"/resources/
-	#fperms a+x "${DESTDIR}"/resources/
-
-	#fowners root "${DESTDIR}/chrome-sandbox"
-	#fperms 4711 "${DESTDIR}/chrome-sandbox"
 
 	dosym ../../opt/"${PN}"/"${MY_PN}" /usr/bin/"${PN}"
 }

commit 79db736dd43ac4c67dd4541340dacc9499f0040a

net-im/legcord-bin: add ${PV} to metainfo.xml source
* Remove trailing period from DESCRIPTION

Bug: https://bugs.gentoo.org/969706
Fixes: 4712478547fb3f23ec2e59fe65cf235a1c29aba9
Signed-off-by: Lucio Sauer <watermanpaint@posteo.net>

net-im/legcord-bin/Manifest
@@ -1,3 +1,3 @@
-DIST app.legcord.Legcord.metainfo.xml 10811 BLAKE2B f64e7ac538bb2e78677ab5dc6d0771390483e38e34fbea397f26361e5b1212fd3c208126f00af4fc3327d4dd7f9cff7c4c56c9915d9fe6630d9a4aa3352a3f63 SHA512 5033ec012c52ab5262015269cce2d262198e71af589886d4de5b5444506c653df82f0347b653c5d658b3e9fc92287eafb8bfbc446242d7a0bf3dd9f6f055b9c5
 DIST legcord-bin-1.2.1-amd64.deb 108194656 BLAKE2B fbb0468322db2b508d260324723bec1d19083df8ba79a985c708d900b85d2767f32e747d3813626ab676ae1666d26ba1416900ea186ffbf89e5925bb80da9072 SHA512 7a6ee9656d71c41a48ab76697bfd3ed4f370fe6a65b8ae8b560f7a48b485f2d4acfefb727a861da9bfb978354529125986538e3ac1e91f5e15682fde970bf17d
 DIST legcord-bin-1.2.1-arm64.deb 103149544 BLAKE2B 3dd75a767608de0e24f10a8c592d05ffa9e81babccf57b551df3ac13709c8c84d951752f203c2c792b9ff23d610e6e1580107158b7bbfebe5f961edb48bd2d07 SHA512 d37ab4e322ab681ccbdff190e87a1e5aa098aedc336f5342ddc747a650dc9515a2fee04883c4f7fe00b672539bf6984c818108103c8c19b330c07add10d6f11a
+DIST legcord-bin-1.2.1.metainfo.xml 10811 BLAKE2B f64e7ac538bb2e78677ab5dc6d0771390483e38e34fbea397f26361e5b1212fd3c208126f00af4fc3327d4dd7f9cff7c4c56c9915d9fe6630d9a4aa3352a3f63 SHA512 5033ec012c52ab5262015269cce2d262198e71af589886d4de5b5444506c653df82f0347b653c5d658b3e9fc92287eafb8bfbc446242d7a0bf3dd9f6f055b9c5

net-im/legcord-bin/legcord-bin-1.2.1.ebuild
@@ -7,7 +7,7 @@ inherit unpacker desktop xdg
 
 MY_PN="Legcord"
 
-DESCRIPTION="Legcord is a custom client designed to enhance your Discord experience."
+DESCRIPTION="Legcord is a custom client designed to enhance your Discord experience"
 HOMEPAGE="https://legcord.app/"
 
 SRC_URI="
@@ -15,7 +15,7 @@ SRC_URI="
 	arm64? ( https://github.com/Legcord/Legcord/releases/download/v${PV}/Legcord-${PV}-linux-arm64.deb -> ${P}-arm64.deb )
 "
 # metainfo
-SRC_URI+="https://github.com/Legcord/Legcord/releases/download/v${PV}/app.legcord.Legcord.metainfo.xml"
+SRC_URI+=" https://github.com/Legcord/Legcord/releases/download/v${PV}/app.legcord.Legcord.metainfo.xml -> ${P}.metainfo.xml"
 
 S="${WORKDIR}"
 
@@ -129,5 +129,5 @@ src_install() {
 	domenu "usr/share/applications/${MY_PN}.desktop"
 
 	insinto /usr/share/metainfo
-	doins "${DISTDIR}/app.legcord.${MY_PN}.metainfo.xml"
+	newins "${DISTDIR}/${P}.metainfo.xml" app.legcord.${MY_PN}.metainfo.xml
 }