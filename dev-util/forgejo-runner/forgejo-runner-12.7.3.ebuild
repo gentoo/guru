gentoo/guru

commit cb6a910c88229ae14f29869a75d085de27a26ba2

acct-*/runner: rename to forgejo-runner
Hasn't hit master yet, so no pkgmove needed.

Drop systemd service file patch. Use sed to inject `PN` and `EPREFIX`.

Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

dev-util/forgejo-runner/forgejo-runner-12.7.3.ebuild
@@ -31,12 +31,8 @@ DEPEND="
 "
 
 RDEPEND="
-	acct-user/runner[docker=,podman=,lxc=]
+	acct-user/${PN}[docker=,podman=,lxc=]
 "
-
-PATCHES=(
-	"${FILESDIR}/${PN}-systemd-rundir.patch"
-)
 
 src_unpack() {
 	if [[ "${PV}" == *9999* ]]; then
@@ -45,6 +41,17 @@ src_unpack() {
 	else
 		go-module_src_unpack
 	fi
+}
+
+src_prepare() {
+	default
+
+	sed \
+		-e "/ExecStart=/s#=.*#=${EPREFIX}/usr/bin/forgejo-runner daemon -c %h/runner-config.yml#g" \
+		-e "/ExecReload=/s#=#=${EPREFIX}#g" \
+		-e "/User=/s#=.*#=${PN}#g" \
+		-e '/WorkingDirectory=/s#=.*#/=~#g' \
+		-i contrib/forgejo-runner.service || die
 }
 
 src_compile() {

dev-util/forgejo-runner/forgejo-runner-9999.ebuild
@@ -31,12 +31,8 @@ DEPEND="
 "
 
 RDEPEND="
-	acct-user/runner[docker=,podman=,lxc=]
+	acct-user/${PN}[docker=,podman=,lxc=]
 "
-
-PATCHES=(
-	"${FILESDIR}/${PN}-systemd-rundir.patch"
-)
 
 src_unpack() {
 	if [[ "${PV}" == *9999* ]]; then
@@ -45,6 +41,17 @@ src_unpack() {
 	else
 		go-module_src_unpack
 	fi
+}
+
+src_prepare() {
+	default
+
+	sed \
+		-e "/ExecStart=/s#=.*#=${EPREFIX}/usr/bin/forgejo-runner daemon -c %h/runner-config.yml#g" \
+		-e "/ExecReload=/s#=#=${EPREFIX}#g" \
+		-e "/User=/s#=.*#=${PN}#g" \
+		-e '/WorkingDirectory=/s#=.*#/=~#g' \
+		-i contrib/forgejo-runner.service || die
 }
 
 src_compile() {

acct-user/forgejo-runner/forgejo-runner-0.ebuild
@@ -9,9 +9,9 @@ DESCRIPTION="Forgejo-Runner user account"
 
 IUSE="docker podman lxc"
 
-ACCT_USER_HOME=/var/lib/runner
+ACCT_USER_HOME="/var/lib/${PN}"
 ACCT_USER_ID=-1
-ACCT_USER_GROUPS=( runner )
+ACCT_USER_GROUPS=( "${PN}" )
 
 acct-user_add_deps
 

dev-util/forgejo-runner/files/forgejo-runner-systemd-rundir.patch
@@ -1,15 +1,0 @@
-diff --git a/contrib/forgejo-runner.service b/contrib/forgejo-runner.service
-index 785d35df..0cbe146d 100644
---- a/contrib/forgejo-runner.service
-+++ b/contrib/forgejo-runner.service
-@@ -8,8 +8,8 @@ ExecStart=/usr/local/bin/forgejo-runner daemon
- ExecReload=/bin/kill -s HUP $MAINPID
- 
- # This user and working directory must already exist
--User=runner 
--WorkingDirectory=/home/runner
-+User=runner
-+WorkingDirectory=/var/lib/runner
- Restart=on-failure
- TimeoutSec=0
- RestartSec=10

commit 87074ab42de2a599dd045ab59b5cd13781f233a1

dev-util/forgejo-runner: fix sed typo
Fixes: cb6a910c88229ae14f29869a75d085de27a26ba2
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

dev-util/forgejo-runner/forgejo-runner-12.7.3.ebuild
@@ -50,7 +50,7 @@ src_prepare() {
 		-e "/ExecStart=/s#=.*#=${EPREFIX}/usr/bin/forgejo-runner daemon -c %h/runner-config.yml#g" \
 		-e "/ExecReload=/s#=#=${EPREFIX}#g" \
 		-e "/User=/s#=.*#=${PN}#g" \
-		-e '/WorkingDirectory=/s#=.*#/=~#g' \
+		-e '/WorkingDirectory=/s#=.*#=~#g' \
 		-i contrib/forgejo-runner.service || die
 }
 

dev-util/forgejo-runner/forgejo-runner-9999.ebuild
@@ -50,7 +50,7 @@ src_prepare() {
 		-e "/ExecStart=/s#=.*#=${EPREFIX}/usr/bin/forgejo-runner daemon -c %h/runner-config.yml#g" \
 		-e "/ExecReload=/s#=#=${EPREFIX}#g" \
 		-e "/User=/s#=.*#=${PN}#g" \
-		-e '/WorkingDirectory=/s#=.*#/=~#g' \
+		-e '/WorkingDirectory=/s#=.*#=~#g' \
 		-i contrib/forgejo-runner.service || die
 }
 