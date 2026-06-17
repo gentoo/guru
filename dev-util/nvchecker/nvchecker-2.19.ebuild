gentoo/guru

commit 68fbddeaec33c84681344a53d9ce231bc2df3348

*/*: drop py3.11 & remove dead py impls
Part-of: https://github.com/gentoo/guru/pull/447
Closes: https://github.com/gentoo/guru/pull/447
Signed-off-by: David Roman <stkw0@disroot.org>

dev-util/nvchecker/nvchecker-2.19.ebuild
@@ -2,7 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
-PYTHON_COMPAT=( python3_{11..14} )
+PYTHON_COMPAT=( python3_{12..14} )
 DISTUTILS_USE_PEP517=setuptools
 inherit distutils-r1 optfeature
 

dev-util/nvchecker/nvchecker-2.20.ebuild
@@ -3,7 +3,7 @@
 
 EAPI=8
 
-PYTHON_COMPAT=( python3_{11..14} )
+PYTHON_COMPAT=( python3_{12..14} )
 DISTUTILS_USE_PEP517=setuptools
 inherit distutils-r1 optfeature
 

commit 5a380e5dc78edb7b0019f7e261851bb2ed3aeb58

dev-util/nvchecker: drop obsolete zstandard handling
Fixes: 68fbddeaec33c84681344a53d9ce231bc2df3348
Closes: https://bugs.gentoo.org/974956
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

dev-util/nvchecker/nvchecker-2.19.ebuild
@@ -2,6 +2,7 @@
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
+
 PYTHON_COMPAT=( python3_{12..14} )
 DISTUTILS_USE_PEP517=setuptools
 inherit distutils-r1 optfeature
@@ -34,7 +35,7 @@ BDEPEND="
 		dev-python/pytest-asyncio[${PYTHON_USEDEP}]
 		dev-python/pytest-httpbin[${PYTHON_USEDEP}]
 		$(python_gen_cond_dep \
-			'dev-python/zstandard[${PYTHON_USEDEP}]' python3_{11..13})
+			'dev-python/zstandard[${PYTHON_USEDEP}]')
 	)
 "
 
@@ -100,9 +101,5 @@ pkg_postinst() {
 	optfeature "jq source" "dev-python/jq[${PYTHON_USEDEP}]"
 	optfeature "httpheader source" "dev-python/lxml[${PYTHON_USEDEP}]"
 	optfeature "pypi source" "dev-python/packaging[${PYTHON_USEDEP}]"
-	if use python_targets_python3_11 || \
-		use python_targets_python3_12 || \
-		use python_targets_python3_13; then
-		optfeature "rpmrepo source" "dev-python/zstandard[${PYTHON_USEDEP}]"
-	fi
+	optfeature "rpmrepo source" "dev-python/zstandard[${PYTHON_USEDEP}]"
 }

dev-util/nvchecker/nvchecker-2.20.ebuild
@@ -35,7 +35,7 @@ BDEPEND="
 		dev-python/pytest-asyncio[${PYTHON_USEDEP}]
 		dev-python/pytest-httpbin[${PYTHON_USEDEP}]
 		$(python_gen_cond_dep \
-			'dev-python/zstandard[${PYTHON_USEDEP}]' python3_{11..13})
+			'dev-python/zstandard[${PYTHON_USEDEP}]')
 		dev-vcs/git
 	)
 "
@@ -98,10 +98,6 @@ pkg_postinst() {
 	optfeature "jq source" "dev-python/jq[${PYTHON_USEDEP}]"
 	optfeature "httpheader source" "dev-python/lxml[${PYTHON_USEDEP}]"
 	optfeature "pypi source" "dev-python/packaging[${PYTHON_USEDEP}]"
-	if use python_targets_python3_11 || \
-		use python_targets_python3_12 || \
-		use python_targets_python3_13; then
-		optfeature "rpmrepo source" "dev-python/zstandard[${PYTHON_USEDEP}]"
-	fi
+	optfeature "rpmrepo source" "dev-python/zstandard[${PYTHON_USEDEP}]"
 	optfeature "git source" dev-vcs/git
 }